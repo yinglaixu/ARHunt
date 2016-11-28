f1742fa9406dbd9745e5006170a6c78a647e01a2
YInglai Jo <joyinglai@qq.com>
2016-11-28 14:47:56 +0100

    working in progress, put models in scene

--- app/app.js
+++ app/app.js
@@ -49,21 +49,77 @@ var locationElement = document.getElementById("location");
 // that we will attach to the geospatial object when we create it.
 // Box texture from https://www.flickr.com/photos/photoshoproadmap/8640003215/sizes/l/in/photostream/
 // licensed under https://creativecommons.org/licenses/by/2.0/legalcode
+
 var boxGeoObject = new THREE.Object3D();
 var box = new THREE.Object3D();
-var loader = new THREE.TextureLoader();
-loader.load('box.png', function (texture) {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ map: texture });
-    var mesh = new THREE.Mesh(geometry, material);
-    box.add(mesh);
+
+var keyGeoObject = new THREE.Object3D();
+var key = new THREE.Object3D();
+
+// loader.load('box.png', function (texture) {
+//     var geometry = new THREE.BoxGeometry(1, 1, 1);
+//     var material = new THREE.MeshBasicMaterial({ map: texture });
+//     var mesh = new THREE.Mesh(geometry, material);
+//     box.add(mesh);
+// });
+
+// model
+var onProgress = function ( xhr ) {
+    if ( xhr.lengthComputable ) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        console.log( Math.round(percentComplete, 2) + '% downloaded' );
+    }
+};
+var onError = function ( xhr ) { };
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.load( "dataset/treasure_chest.mtl", function( materials ) {
+    materials.preload();
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials( materials );
+    objLoader.load("dataset/treasure_chest.obj", function ( object ) {
+        object.rotation.y = 180* Math.PI / 180;
+        object.scale.x = 5;
+        object.scale.y = 5;
+        object.scale.z = 5;
+        box.add(object);
+    }, onProgress, onError );
 });
 boxGeoObject.add(box);
+
+mtlLoader.load( "dataset/Key_B_02.mtl", function( materials ) {
+    materials.preload();
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials( materials );
+    objLoader.load("dataset/Key_B_02.obj", function ( object ) {
+        object.rotation.y = 180* Math.PI / 180;
+        object.scale.x = 5;
+        object.scale.y = 5;
+        object.scale.z = 5;
+        key.add(object);
+    }, onProgress, onError );
+});
+keyGeoObject.add(key);
+
+
+
 var boxGeoEntity = new Argon.Cesium.Entity({
     name: "I have a box",
     position: Cartesian3.ZERO,
     orientation: Cesium.Quaternion.IDENTITY
 });
+
+var keyGeoEntity = new Argon.Cesium.Entity({
+    name: "I have a key",
+    position: Cartesian3.ZERO,
+    orientation: Cesium.Quaternion.IDENTITY
+});
+
+
+
+
+
+
 // the updateEvent is called each time the 3D world should be
 // rendered, before the renderEvent.  The state of your application
 // should be updated here.
@@ -109,6 +165,53 @@ app.updateEvent.addEventListener(function (frame) {
     // to make it a little less boring
     box.rotateY(3 * frame.deltaTime / 10000);
 });
+
+// the updateEvent is called each time the 3D world should be
+// rendered, before the renderEvent.  The state of your application
+// should be updated here.
+var keyInit = false;
+app.updateEvent.addEventListener(function (frame) {
+    // get the position and orientation (the "pose") of the user
+    // in the local coordinate frame.
+    var userPose = app.context.getEntityPose(app.context.user);
+    // assuming we know the user's pose, set the position of our 
+    // THREE user object to match it
+    if (userPose.poseStatus & Argon.PoseStatus.KNOWN) {
+        userLocation.position.copy(userPose.position);
+    }
+    else {
+        // if we don't know the user pose we can't do anything
+        return;
+    }
+    // the first time through, we create a geospatial position for
+    // the box somewhere near us 
+    if (!keyInit) {
+        var defaultFrame = app.context.getDefaultReferenceFrame();
+        // set the box's position to 10 meters away from the user.
+        // First, clone the userPose postion, and add 10 to the X
+        var keyPos = userPose.position.clone();
+        keyPos.x -= 10;
+        // set the value of the box Entity to this local position, by
+        // specifying the frame of reference to our local frame
+        keyGeoEntity.position.setValue(keyPos, defaultFrame);
+        // orient the box according to the local world frame
+        keyGeoEntity.orientation.setValue(Cesium.Quaternion.IDENTITY);
+        // now, we want to move the box's coordinates to the FIXED frame, so
+        // the box doesn't move if the local coordinate system origin changes.
+        if (Argon.convertEntityReferenceFrame(keyGeoEntity, frame.time, ReferenceFrame.FIXED)) {
+            scene.add(keyGeoObject);
+            keyInit = true;
+        }
+    }
+    // get the local coordinates of the local box, and set the THREE object
+    var keyPose = app.context.getEntityPose(keyGeoEntity);
+    keyGeoObject.position.copy(keyPose.position);
+    keyGeoObject.quaternion.copy(keyPose.orientation);
+    // rotate the boxes at a constant speed, independent of frame rates     
+    // to make it a little less boring
+    key.rotateY(3 * frame.deltaTime / 10000);
+});
+
 // renderEvent is fired whenever argon wants the app to update its display
 app.renderEvent.addEventListener(function () {
     // set the renderers to know the current size of the viewport.
